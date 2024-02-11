acee410076dc1ec3a77bb6d895070d6c61135b57
Dustin Toff <me@dustintoff.com>
2024-02-10 21:01:29 -0500

    Extern all @aws-sdk/client-*

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require("path");
 
+function getAwsSdkExternals() {
+    const packageJson = require("./package.json");
+    return Object.fromEntries(
+        Object.keys(packageJson["devDependencies"])
+            .filter((key) => key.startsWith("@aws-sdk/client-"))
+            .map((entry) => [entry, entry])
+    );
+}
+
 module.exports = {
     entry: "./src/index.ts",
     output: {
@@ -7,6 +16,10 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         asyncChunks: false,
     },
+    externals: {
+        ...getAwsSdkExternals(),
+    },
+    externalsType: "commonjs",
     mode: "production",
     target: "node",
     module: {
